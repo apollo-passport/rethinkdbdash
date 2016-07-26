80f05d5a6b86cf368b9640833c8e2d8d8111aeec
Gadi Cohen <dragon@wastelands.net>
2016-07-26 16:28:50 +0300

    v0.0.1 and a little more coverage

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import sinon from 'sinon';
 import 'regenerator-runtime/runtime';
 
 import RethinkDBDashDriver from './index';
@@ -50,9 +51,31 @@ describe('apollo-passport-rethinkdbdash', () => {
 
   const db = new RethinkDBDashDriver(r);
 
+  describe('constructor()', () => {
+
+    it('stores r', () => {
+      const r = { table(x) { return 'table:'+x; } };
+      const db = new RethinkDBDashDriver(r);
+      db.r.should.equal(r);
+    });
+
+    it('uses a default table name', () => {
+      const r = { table(x) { return 'table:'+x; } };
+      const db = new RethinkDBDashDriver(r);
+      db.users.should.equal('table:users');
+    });
+
+    it('accepts a table name', () => {
+      const r = { table(x) { return 'table:'+x; } };
+      const db = new RethinkDBDashDriver(r, 'sheeple');
+      db.users.should.equal('table:sheeple');
+    });
+
+  });
+
   describe('createUser()', () => {
 
-    it('inserts a user and returns the id', async () => {
+    it('inserts a user and returns the id when no id given', async () => {
       await freshUserTable();
 
       const user = { name: 'John Sheppard' };
@@ -64,6 +87,20 @@ describe('apollo-passport-rethinkdbdash', () => {
       added.name.should.equal(user.name);
       id.should.equal(added.id);
     });
+
+    it('inserts a user and returns the id when an is id given', async () => {
+      await freshUserTable();
+
+      const user = { id: 'sheppard', name: 'John Sheppard' };
+      const id = await db.createUser(user);
+
+      const users = await db.users.run();
+      const added = users[0];
+
+      added.name.should.equal(user.name);
+      id.should.equal(added.id);
+    });
+
 
   });
 
